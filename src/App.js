6221a2fa6780a92b2d7228be328ded17d892b1e4
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-11-07 14:37:53 -0500

    Added transition effect to pages switch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { AnimatePresence } from 'framer-motion';
+
+
 
 function App() {
+    const location = useLocation();
 
     return (
         <>
+            <Navbar />
             <Router>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={About} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/portfolio" component={Portfolio} />
-                    <Route exact path="/contact" component={Contact} />
-                </Switch>
-                {/* <Footer /> */}
+                <AnimatePresence exitBeforeEnter>
+                    <Switch location={location} key={location.pathname}>
+                        <Route exact path="/" component={About} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/portfolio" component={Portfolio} />
+                        <Route exact path="/contact" component={Contact} />
+                    </Switch>
+                </AnimatePresence>
             </Router>
         </>
     );
--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -4,100 +4,107 @@ import Resume from "../../assets/pdf/Resume.pdf"
 import { motion } from "framer-motion";
 import NextButton from "../../nextButton";
 
-function About(){
+function About() {
 
-    return (
-    <article id="about">
-        <div className="row">
-            <section className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <div id="portrait-container">
-                    <motion.div
-                        id="motionDivTwo"
-                        animate={{ borderRadius: ["38% 68% 32% 62%", "58% 38% 72% 42%", "45% 35% 55% 65%"] }}
-                        transition={{
-                            duration: 12,
-                            yoyo: Infinity,
-                            ease: "easeInOut"
-                        }}
-                    >
+    return (<motion.div
+        initial={{ opacity: 0, x: 0, y: 200 }}
+        animate={{ opacity: 1, x: 0, y: 0 }}
+        exit={{ opacity: 0, x: 0, y: -200 }}
+        transition={{ duration: 0.5 }}
+    >
+        <article id="about">
+            <div className="row">
+                <section className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div id="portrait-container">
                         <motion.div
-                            id="motionDivOne"
-                            animate={{ borderRadius: ["38% 68% 32% 62%", "55% 35% 75% 55%", "58% 38% 72% 42%"] }}
+                            id="motionDivTwo"
+                            animate={{ borderRadius: ["38% 68% 32% 62%", "58% 38% 72% 42%", "45% 35% 55% 65%"] }}
                             transition={{
-                                duration: 10,
+                                duration: 12,
                                 yoyo: Infinity,
                                 ease: "easeInOut"
                             }}
                         >
-                            <motion.img src={process.env.PUBLIC_URL + "/images/bw-portrait.jpg"} alt="Portrait of Manuel" id="portrait-image"
-                                animate={{ borderRadius: ["35% 45% 50% 75%", "38% 68% 32% 62%", "58% 38% 72% 42%"] }}
+                            <motion.div
+                                id="motionDivOne"
+                                animate={{ borderRadius: ["38% 68% 32% 62%", "55% 35% 75% 55%", "58% 38% 72% 42%"] }}
                                 transition={{
-                                    duration: 15,
+                                    duration: 10,
                                     yoyo: Infinity,
                                     ease: "easeInOut"
                                 }}
-                            ></motion.img>
+                            >
+                                <motion.img src={process.env.PUBLIC_URL + "/images/bw-portrait.jpg"} alt="Portrait of Manuel" id="portrait-image"
+                                    animate={{ borderRadius: ["35% 45% 50% 75%", "38% 68% 32% 62%", "58% 38% 72% 42%"] }}
+                                    transition={{
+                                        duration: 15,
+                                        yoyo: Infinity,
+                                        ease: "easeInOut"
+                                    }}
+                                ></motion.img>
+                            </motion.div>
                         </motion.div>
-                    </motion.div>
-                </div>
-            </section>
-            <section className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <div id="about-me">
-                    <h3>ABOUT ME</h3>
-                    <hr></hr>
-                    <p>
-                        I'm Manuel, a Full-stack Developer originally from the Dominican Republic and currently based in Toronto, Canada.  After realizing how computer devices positively changed our lifestyles, I fell in love with technology. That led to an Advanced Diploma in Computer Engineering Technology, and I've now worked in IT for three years.
-                    </p>
-                    <p>
-                        With almost five years in the customer service industry and encountering multiple user errors along the way, I'm now putting together all my experiences and offering thoughtfully designed user-friendly applications and all with a positive attitude.
-                    </p>
-                    <p>Phone: (647)-704-2246</p>
-                    <p>Email: manuel.leungchen@gmail.com</p>
-                </div>
-                <div className="row" id="social">
-                    <div className="col-3">
-                    <a href="https://www.linkedin.com/in/manuel-leung-chen/" rel="noreferrer" target="_blank">
-                        <i className="fab fa-linkedin"></i></a>
                     </div>
-                    <div className="col-3">
-                        <a href="https://github.com/manuelleungchen" rel="noreferrer" target="_blank">
-                        <i className="fab fa-github"></i></a>
+                </section>
+                <section className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div id="about-me">
+                        <h3>ABOUT ME</h3>
+                        <hr></hr>
+                        <p>
+                            I'm Manuel, a Full-stack Developer originally from the Dominican Republic and currently based in Toronto, Canada.  After realizing how computer devices positively changed our lifestyles, I fell in love with technology. That led to an Advanced Diploma in Computer Engineering Technology, and I've now worked in IT for three years.
+                        </p>
+                        <p>
+                            With almost five years in the customer service industry and encountering multiple user errors along the way, I'm now putting together all my experiences and offering thoughtfully designed user-friendly applications and all with a positive attitude.
+                        </p>
+                        <p>Phone: (647)-704-2246</p>
+                        <p>Email: manuel.leungchen@gmail.com</p>
+                    </div>
+                    <div className="row" id="social">
+                        <div className="col-3">
+                            <a href="https://www.linkedin.com/in/manuel-leung-chen/" rel="noreferrer" target="_blank">
+                                <i className="fab fa-linkedin"></i></a>
                         </div>
-                    <div className="col-3">
-                    <a href="https://www.instagram.com/mleungc/" rel="noreferrer" target="_blank">
-                        <i className="fab fa-instagram"></i></a>
+                        <div className="col-3">
+                            <a href="https://github.com/manuelleungchen" rel="noreferrer" target="_blank">
+                                <i className="fab fa-github"></i></a>
+                        </div>
+                        <div className="col-3">
+                            <a href="https://www.instagram.com/mleungc/" rel="noreferrer" target="_blank">
+                                <i className="fab fa-instagram"></i></a>
+                        </div>
+                        <div className="col-3">
+                            <a href={Resume} rel="noreferrer" target="_blank">
+                                <i className="far fa-file-pdf"></i></a>
+                        </div>
                     </div>
-                    <div className="col-3">
-                    <a href={Resume} rel="noreferrer" target="_blank">
-                        <i className="far fa-file-pdf"></i></a>
+                </section>
+                <section className="col-12">
+                    <hr></hr>
+                    <div id="skills">
+                        <h3>SKILLS</h3>
+                        <div className="row" id="skill-icons">
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-html5"></i><p>HTML 5</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-css3-alt"></i><p>CSS</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-js-square"></i><p>JavaScript</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-node-js"></i><p>Node.js</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-npm"></i><p>NPM</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-react"></i><p>React.js</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-universal-access"></i><p>Accessability</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-github"></i><p>Github</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-mobile-alt"></i><p>Responsive</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-database"></i><p>MongoDB</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-server"></i><p>Heroku</p></div>
+                            <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-database"></i><p>MySQL</p></div>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="col-12">
-            <hr></hr>
-                <div id="skills">
-                    <h3>SKILLS</h3>
-                    <div className="row" id="skill-icons">
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-html5"></i><p>HTML 5</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-css3-alt"></i><p>CSS</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-js-square"></i><p>JavaScript</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-node-js"></i><p>Node.js</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-npm"></i><p>NPM</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-react"></i><p>React.js</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-universal-access"></i><p>Accessability</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fab fa-github"></i><p>Github</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-mobile-alt"></i><p>Responsive</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-database"></i><p>MongoDB</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-server"></i><p>Heroku</p></div>
-                        <div className="col-3 col-md-2 col-lg-2 col-xl-1"><i className="fas fa-database"></i><p>MySQL</p></div>
-                    </div>
-                </div>
-            </section>
-            <section className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <NextButton nextPage="portfolio" ></NextButton>
-            </section>
-        </div>
-    </article>
-)}
+                </section>
+                <section className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <NextButton nextPage="portfolio" ></NextButton>
+                </section>
+            </div>
+        </article>
+    </motion.div>
+    )
+}
 
 export default About;
